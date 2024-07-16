dbad909e269eccf60c444b3dafd7bd93f991080e
Vishal Antil <antilvishal2402@gmail.com>
2024-07-17 03:15:25 +0530

    Book my show starting

---
+++ BookmyShow/src/main/java/com/book_my_show/BookmyShow/BookmyShowApplication.java
@@ -0,0 +1,13 @@
+package com.book_my_show.BookmyShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmyShowApplication.class, args);
+	}
+
+}
---
+++ BookmyShow/src/test/java/com/book_my_show/BookmyShow/BookmyShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book_my_show.BookmyShow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
